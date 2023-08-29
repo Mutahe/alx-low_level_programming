52914a282747b0857eab0d07b5856a05b690e3bb
Mutahe <elizagatura@gmail.com>
2023-08-29 02:50:40 -0700

    task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,10 +7,10 @@
  *
  * Return: the number of nodes
  */
-size_t print_listint(const listint_t *h);
-
-size_t num = 0;
+size_t print_listint(const listint_t *h)
 {
+	size_t num = 0;
+
 	while(h != NULL)
 	{
 		printf("%d\n", h->n);
