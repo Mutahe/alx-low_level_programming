4a99ad2add5164c06bc9e0919e30df23995f1266
Mutahe <elizagatura@gmail.com>
2023-08-29 15:57:11 -0700

    com

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -10,15 +10,14 @@
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *first_node =(listint_t *)malloc(sizeof(listint_t));
-	
+	listint_t *first_node = (listint_t *)malloc(sizeof(listint_t));
 	if (first_node == NULL)
 	{
-  		return (NULL);
+		return (NULL);
 	}
 	first_node->n = n;
 	first_node->next = *head;
-	*head =first_node;
+	*head = first_node;
 
 	return (first_node);
 }
