9c97353a8ec616f3259313f84c616289f48de7d5
Lizbeth <elizagatura@gmail.com>
2023-09-04 23:10:26 +0300

    task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,12 +8,34 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t = 0
+	int lz;
+	char *buf;
+	ssize_t num;
+	ssize_t let;
 
-		read_textfile = fopen(filename, "j");
-	if (read_textfile == NULL)
-	{
-		perror("Error");
+	if (filename == NULL)
 		return (0);
+	lz = open(filename, O_RDONLY);
+	if (lz == -1)
+		return (0);
+	*buf = (char *)malloc(sizeof char * letters);
+	if (buf == NULL)
+	{
+	close(lz);
+		return (0);
+	num = read(lz, buf, letters);
+	close(lz);
 	}
+	if (num < 0)
+	{
+		free(buf);
+		return (0);
+	let = write(STDOUT_FILENO, buf, num);
+	free(buf);
+	}
+	if (let < 0 || (ssize_t)let != num);
+		return (0);
+	
+	return (num);
 }
+
