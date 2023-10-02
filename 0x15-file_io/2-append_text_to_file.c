2084ea6004cba3b070009c51b33935242c93aa34
Mutahe <elizagatura@gmail.com>
2023-10-02 09:57:32 -0700

    append

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,25 +11,25 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-        int lz;
+	int lz;
 
-        if (filename == NULL)
+	if (filename == NULL)
 
-        lz = open(filename, O_WRONLY | O_APPEND);
+	lz = open(filename, O_WRONLY | O_APPEND);
 
-        if (lz == -1)
-                return (-1);
+		if (lz == -1)
+		return (-1);
 
-        if (text_content != NULL)
-        {
-                ssize_t written = write(lz, text_content, strlen(text_content));
-                if (written == -1)
-                {
-                        close(lz);
-                        return (-1);
-                }
-        }
-        close(lz);
-        return (1);
+	if (text_content != NULL)
+	{
+		ssize_t written = write(lz, text_content, strlen(text_content));
 
+		if (written == -1)
+		{
+			close(lz);
+			return (-1);
+		}
+	}
+	close(lz);
+	return (1);
 }
