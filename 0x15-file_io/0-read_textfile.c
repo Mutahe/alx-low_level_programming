5c3fc29c666036ad6168bc29a6b3a70aae63ef98
Mutahe <elizagatura@gmail.com>
2023-10-02 08:33:58 -0700

    Input output

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: pointer to number of letters
+ * @letters: length of letters
+ * Return: actual number of letters it could read and print, else return 0
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+        int lz;
+        char *buf;
+        ssize_t num, let;
+
+        if (filename == NULL)
+                return (0);
+
+        lz = open(filename, O_RDONLY);
+        if (lz == -1)
+                return (0);
+
+        buf = (char *)malloc(sizeof(char) * letters);
+        if (buf == NULL)
+        {
+                close(lz);
+                return (0);
+        }
+        num = read(lz, buf, letters);
+
+        close(lz);
+
+        if (num < 0)
+        {
+                free(buf);
+                return (0);
+        }
+        let = write(STDOUT_FILENO, buf, num);
+
+        free(buf);
+
+                if (let < 0 || (ssize_t)let != num)
+                return (0);
+
+        return (num);
+}
---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * create_file.c - used to create a file
+ * @filename: points to chracter
+ * @text_content: points to the content
+ *
+ * Return: 1 on success
+ *         -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+        int lz;
+
+        if (filename == NULL)
+                return (-1);
+        lz = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+
+        if (lz == -1)
+                return (-1);
+        if (text_content != NULL)
+        {
+                ssize_t written = write(lz, text_content, strlen(text_content));
+
+                if (written == -1)
+                {
+                        close(lz);
+                        return (-1);
+                }
+
+        }
+        close(lz);
+
+        return (1);
+}
