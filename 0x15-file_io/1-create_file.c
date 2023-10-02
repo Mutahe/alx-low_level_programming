dce7ce822c430c55775dab7386faeb4b48c3f855
Mutahe <elizagatura@gmail.com>
2023-10-02 08:56:04 -0700

    creation

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * create_file.c - used to create a file
+ * create_file - used to create a file
  * @filename: points to chracter
  * @text_content: points to the content
  *
@@ -10,26 +10,24 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-        int lz;
+	int lz;
 
-        if (filename == NULL)
-                return (-1);
-        lz = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (filename == NULL)
+		return (-1);
+	lz = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (lz == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		ssize_t written = write(lz, text_content, strlen(text_content));
+			if (written == -1)
+			{
+				close(lz);
+				return (-1);
+			}
 
-        if (lz == -1)
-                return (-1);
-        if (text_content != NULL)
-        {
-                ssize_t written = write(lz, text_content, strlen(text_content));
+	}
+	close(lz);
 
-                if (written == -1)
-                {
-                        close(lz);
-                        return (-1);
-                }
-
-        }
-        close(lz);
-
-        return (1);
+	return (1);
 }
