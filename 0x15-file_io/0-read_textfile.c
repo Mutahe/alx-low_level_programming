8e5c2adc5dfa96c582d10bb9bca366c7a907fc2d
Mutahe <elizagatura@gmail.com>
2023-10-02 08:40:42 -0700

    input output

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,38 +8,38 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-        int lz;
-        char *buf;
-        ssize_t num, let;
+	int lz;
+	char *buf;
+	ssize_t num, let;
 
-        if (filename == NULL)
-                return (0);
+	if (filename == NULL)
+		return (0);
 
-        lz = open(filename, O_RDONLY);
-        if (lz == -1)
-                return (0);
+	lz = open(filename, O_RDONLY);
+	if (lz == -1)
+		return (0);
 
-        buf = (char *)malloc(sizeof(char) * letters);
-        if (buf == NULL)
-        {
-                close(lz);
-                return (0);
-        }
-        num = read(lz, buf, letters);
+	buf = (char *)malloc(sizeof(char) * letters);
+	if (buf == NULL)
+	{
+		close(lz);
+		return (0);
+	}
+	num = read(lz, buf, letters);
 
-        close(lz);
+	close(lz);
 
-        if (num < 0)
-        {
-                free(buf);
-                return (0);
-        }
-        let = write(STDOUT_FILENO, buf, num);
+	if (num < 0)
+	{
+		free(buf);
+		return (0);
+	}
+	let = write(STDOUT_FILENO, buf, num);
 
-        free(buf);
-
-                if (let < 0 || (ssize_t)let != num)
-                return (0);
-
-        return (num);
+	free(buf);
+	
+	if (let < 0 || (ssize_t)let != num)
+		return (0);
+	
+	return (num);
 }
