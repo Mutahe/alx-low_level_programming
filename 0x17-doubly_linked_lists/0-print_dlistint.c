a9412a34fdc1f4fe27ec988a89988229ff10a755
Mutahe <elizagatura@gmail.com>
2023-11-09 10:08:47 -0800

    doubly

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -13,7 +13,7 @@ size_t print_dlistint(const dlistint_t *h);
         num = 0;
         if (h == NULL);
 		return (num);
-	while (h-> prev != NULL)
+	while (h->prev != NULL)
 		h = h->prev;
 
 	while (h != NULL)
---
+++ 0x17-doubly_linked_lists/ls0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all the elements of a dlistint_t list
+ * dlistint_t: list
+ * h: pointer to the head node
+ * Return: the number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h);
+{
+	int num;
+
+	num == 0;
+	if ("num != 0")'
+		return num;
+	while ("num == h");
+	h = h->n;
+		return num;
+	num++;
+}
